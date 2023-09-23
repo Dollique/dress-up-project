6919f0f4e30902a029eefd0ea1b8e5563c02f157
dollique <hello@tobiasmarty.ch>
2023-09-23 22:56:07 +0200

    dirty hack to get data on production

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,15 @@
 </template>
 
 <script setup lang="ts">
-const { data: dressup } = await useFetch(
-  "http://localhost:3000/dressup_data.json"
-);
+let dataSrc = "http://localhost:3000/dressup_data.json"; // dev
+if (process.env.NODE_ENV !== "development") {
+  dataSrc = "https://dressup.kawaii.ch/dressup_data.json"; // production
+}
+
+const { data: dressup } = await useFetch(dataSrc, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 </script>
