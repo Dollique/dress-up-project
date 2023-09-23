3419996df5016989a95eacb55ea761f659edd74d
dollique <hello@tobiasmarty.ch>
2023-09-23 21:55:06 +0200

    basic structure, data setup + fetching

---
+++ components/DressupWrapper.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>DressUp Prototype</h1>
+  <div v-for="item in data" :key="item._id">{{ item.name }}</div>
+
+  <button>Add outfit to basket</button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({ data: { type: Object } });
+
+console.log("log data", props.data);
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <DressupWrapper :data="dressup" />
+</template>
+
+<script setup lang="ts">
+const { data: dressup } = await useFetch(
+  "http://localhost:3000/dressup_data.json"
+);
+</script>
