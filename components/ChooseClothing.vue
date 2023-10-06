973a77e07e40e56e42bfc0ab8a6439d702caf0de
dollique <hello@tobiasmarty.ch>
2023-10-06 20:03:25 +0200

    implement tabs and new navigation store

--- components/ChooseClothing.vue
+++ components/ChooseClothing.vue
@@ -1,70 +1,92 @@
 <template>
-  <section class="itemCard">
-    <article class="prose">
-      <h2>Choose {{ piece }}</h2>
-    </article>
+  <article class="prose">
+    <h2>Choose {{ category }}</h2>
+  </article>
 
-    <div class="flex flex-wrap gap-4">
-      <div
-        v-for="item in dataPiece"
-        :key="item._id"
-        :data-id="item._id"
-        class="item"
-        @click="selectItem"
-      >
-        <img :src="`/images/clothing/${item.image}`" />
-        <div class="">{{ item.name }}</div>
-      </div>
+  <div class="flex flex-wrap gap-4">
+    <div
+      v-for="item in getItems"
+      :key="item._id"
+      :data-id="item._id"
+      :class="`item ${
+        item._id === productsStore[`${category}_id`] ? 'active' : ''
+      }`"
+      @click="selectItem"
+    >
+      <img :src="`/images/clothing/${item.image}`" />
+      <div class="">{{ item.name }}</div>
     </div>
-    <div>Selected: {{ productsStore[`${props.piece}_id`] }}</div>
+  </div>
+  <div>Selected: {{ productsStore[`${category}_id`] }}</div>
 
-    <select>
-      <option>Select size</option>
-      <option v-for="(size, index) in sizes" :key="`${size}_${index}`">
-        {{ size }}
-      </option>
-    </select>
+  <select @change="handleSelect($event, 'size')">
+    <option>Select size</option>
+    <option v-for="(size, index) in sizes" :key="`${size}_${index}`">
+      {{ size }}
+    </option>
+  </select>
 
-    <select class="mt-1">
-      <option>Select length</option>
-      <option v-for="(length, index) in lengths" :key="`${length}_${index}`">
-        {{ length }}
-      </option>
-    </select>
-  </section>
+  <select @change="handleSelect($event, 'length')" class="mt-1">
+    <option>Select length</option>
+    <option v-for="(length, index) in lengths" :key="`${length}_${index}`">
+      {{ length }}
+    </option>
+  </select>
 </template>
 
 <script setup lang="ts">
 import { useProductsStore } from "~/store/products";
+import { useNavigationStore } from "~/store/products";
+import { storeToRefs } from "pinia";
 
-const props = defineProps({ data: { type: Object }, piece: { type: String } });
-
-// get only data of the chosen piece category
-const dataPiece = props.data.filter((item) => item.category === props.piece);
+const props = defineProps({ data: { type: Object } });
 
 // get the store data
 const productsStore = useProductsStore();
+
+// get item category
+const navigationStore = useNavigationStore();
+const { category } = storeToRefs(navigationStore);
+
+// get initial data of the chosen category
+const getFilteredItems = (data, dataCategory) => {
+  return data.filter((item) => item.category === dataCategory);
+};
+
+let getItems = getFilteredItems(props.data, category.value);
 
 // define empty variables outside of functio scope
 let sizes = ref([]);
 let lengths = ref([]);
 
+const getSelectFilters = () => {
+  const getItem = props.data.filter(
+    (itm) => itm._id === productsStore[`${category.value}_id`]
+  )[0];
+
+  if (getItem) {
+    sizes = getItem.availableSizes;
+    lengths = getItem.availableLength;
+  }
+};
+
+const handleSelect = (selected, type) => {
+  productsStore[`${category.value}_${type}`] = selected.target.value;
+};
+
+watch(category, (newCategory) => {
+  getItems = getFilteredItems(props.data, newCategory);
+  getSelectFilters();
+});
+
+watch(productsStore, () => {
+  getSelectFilters();
+});
+
+// update store on click
 const selectItem = (event) => {
   const getId = event.currentTarget.getAttribute("data-id");
-  productsStore[`${props.piece}_id`] = parseFloat(getId) as number;
-
-  // toggle active class
-  event.currentTarget.parentNode
-    .querySelectorAll(".item")
-    .forEach((el) => el.classList.remove("active"));
-  event.currentTarget.classList.add("active");
-
-  // const sizes = props.data[itemState.value].availableSizes;
-  const getItem = props.data.filter(
-    (itm) => itm._id === productsStore[`${props.piece}_id`]
-  )[0];
-  sizes = getItem.availableSizes;
-  lengths = getItem.availableLength;
+  productsStore[`${category.value}_id`] = parseFloat(getId) as number;
 };
 </script>
 
