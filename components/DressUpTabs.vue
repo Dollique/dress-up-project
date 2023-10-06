8a4c8b36531a06e3f67235faa1c8583d56b9c836
dollique <hello@tobiasmarty.ch>
2023-10-06 20:00:16 +0200

    new tabs component

---
+++ components/DressUpTabs.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="tabs">
+    <a
+      :class="`tab tab-itemCart ${
+        navigationStore.category === 'top' ? 'tab-active' : ''
+      }`"
+      data-tabs-category="top"
+      @click="handleTabClick"
+      >Shirts</a
+    >
+    <a
+      :class="`tab tab-itemCart ${
+        navigationStore.category === 'bottom' ? 'tab-active' : ''
+      }`"
+      data-tabs-category="bottom"
+      @click="handleTabClick"
+      >Skirts</a
+    >
+    <a
+      :class="`tab tab-itemCart ${
+        navigationStore.category === 'accessoirs' ? 'tab-active' : ''
+      }`"
+      data-tabs-category="accessoirs"
+      @click="handleTabClick"
+      >Accessoirs</a
+    >
+  </div>
+</template>
+
+<script setup>
+import { useNavigationStore } from "~/store/products";
+const navigationStore = useNavigationStore();
+
+const handleTabClick = (event) => {
+  navigationStore.category = event.currentTarget.dataset.tabsCategory;
+};
+</script>
