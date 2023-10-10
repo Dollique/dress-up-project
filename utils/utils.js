dceef6f5b4f5cd6752d771912f318902bf449439
dollique <hello@tobiasmarty.ch>
2023-10-10 15:43:55 +0200

    moved utils to its own file

---
+++ utils/utils.js
@@ -0,0 +1,21 @@
+// checks if an object is empty
+export const isEmpty = (obj) => {
+  for (const prop in obj) {
+    if (Object.hasOwn(obj, prop)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// get active product object by id
+export const getActiveProduct = (productArray, id) => {
+  if (!id) return;
+  const result = productArray.filter((item) => item.id === id);
+  return result[0].attributes;
+};
+
+// remove last char of a string if it's an 's'
+export const strToSingular = (str) => {
+  if (str.slice(-1) === "s") return str.slice(0, -1);
+};
